7597377156d796a9cc460abff8a19ae86b793931
Orkhanvg <100156013+Orkhanvg@users.noreply.github.com>
2022-03-15 17:43:49 +0400

    Add files via upload

---
+++ newmethod/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace newmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+             * int tipindən bir array olur içində müsbət və mənfi ədədlər olur 
+             * elə bir method yazın ki, bu array-i parametr olaraq qəbul etsin 
+             * və qəbul etdiyi array-in əvvəlcədən assign olunması məcburi olsun 
+             * daha sonra içindəki mənfi ədədləri müsbətə çevirib 
+             * həmin array-i geriyə qaytarsın
+             */
+            int[] numArr = { 12, 2321, 321, -3, -2 };
+            int[] newNumArr = numbers(ref numArr);
+            foreach (var item in newNumArr)
+            {
+                Console.WriteLine(item);
+            }
+
+          
+
+        }
+
+        static int[] numbers(ref int[] num)
+        {
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] < 0)
+                {
+                    num[i] *= -1;
+                }
+            }
+
+            return num;
+              
+
+
+        } 
+    }
+}
