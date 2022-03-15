a921bd6c7c8ddb1e3e5c8037de77e354ffed36bd
orkhangahramanli <Orxan0100@hotmail.com>
2022-03-15 17:42:41 +0400

    feat: new method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace newmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
